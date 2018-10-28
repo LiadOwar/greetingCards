57755bcc73d96a4c98f96e2782918fe3d140e3e8
liadm <liadm@emprest.com>
2018-10-28 13:55:31 +0200

    clean homepage ts

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient} from '@angular/common/http';
-import { Headers, RequestOptions } from '@angular/http';
+import {Component, OnInit} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-homepage',
@@ -8,26 +7,18 @@ import { Headers, RequestOptions } from '@angular/http';
   styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
-
-   readonly ROOT_URL = 'http://localhost:8080';
-  templates : any;
-  options : any;
+  readonly ROOT_URL = 'http://localhost:8080';
+  templates: any[] = [];
 
   constructor(private http: HttpClient) {
-  const header = new Headers();
-   header.append('Access-Control-Allow-Origin', 'http://localhost:4200');
-   this.options = new RequestOptions({header: header});
   }
 
   ngOnInit() {
   }
-   getCatalog() {
-      this.templates = this.http.get(this.ROOT_URL + '/greetingCard/catalog', {observe: 'response'})
-      .subscribe(resp => {
-                            this.templates = resp.body;
-                            console.log(this.templates);
-                            return this.templates;
-                           });
-    }
+
+  getCatalog() {
+    const url = this.ROOT_URL + '/greetingCard/catalog';
+    this.http.get(url).subscribe((response) => this.templates = <any[]>response);
+  }
 
 }
