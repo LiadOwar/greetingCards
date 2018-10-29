a10dd377dffd114f1bbd216a76a6efdeccbf268e
liadm <liadm@emprest.com>
2018-10-29 19:55:20 +0200

    implement postCard in controller

--- src/main/java/greetingApp/controllers/GreetingCardsController.java
+++ src/main/java/greetingApp/controllers/GreetingCardsController.java
@@ -1,7 +1,9 @@
 package greetingApp.controllers;
 
+import greetingApp.greetingCardData.AbstractGreetingCardData;
 import greetingApp.services.GreetingCardService;
 import greetingApp.services.GreetingCardTemplate;
+import greetingApp.services.ViewToModelConvertor;
 import greetingApp.viewmodel.GreetingCardViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +17,8 @@ import java.util.List;
 public class GreetingCardsController {
     @Autowired
     private GreetingCardService greetingCardService;
+    @Autowired
+    private ViewToModelConvertor convertor;
 
     @CrossOrigin
     @RequestMapping("/greetingCard/catalog")
@@ -27,6 +31,8 @@ public class GreetingCardsController {
     @RequestMapping(method= RequestMethod.POST, value ="/greetingCard/createCard")
     public Boolean createCard(@RequestBody GreetingCardViewModel greetingCardViewModel) throws Exception{
         System.out.println(String.format("got post request with body: [%s]", greetingCardViewModel));
+        AbstractGreetingCardData cardData = convertor.convertGreetingCardViewModel(greetingCardViewModel);
+        greetingCardService.postCard(cardData);
 
 
         return true;
